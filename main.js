0745dca8f8a7d5d1a78a704f6b9f94cf978eecdb
kingkong1106 <78448474+kingkong1106@users.noreply.github.com>
2021-10-09 13:47:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -36,8 +36,8 @@ speak();
  Webcam.set({
     width: 360,
     height: 250,
-    image_format: 'png',
-    png_quality: 90
+    image_format: 'jpeg',
+    jpeg_quality: 90
  });
    
 function take_snapshot()
