458132936f27834138ed31779fe933ecb16ef8e0
kingkong1106 <78448474+kingkong1106@users.noreply.github.com>
2021-10-09 13:40:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ speak();
 function take_snapshot()
 {
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = "<img id='selfie_image' src='" + data_uri + "'>";
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="' + data_uri + '">';
 
     });
 }
